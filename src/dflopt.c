bac0c6ebcb303c40e41c922b355e5ec82d9e6b7a
Johan de Claville Christiansen <johan@space-inventor.com>
2022-05-16 18:38:00 +0200

    Add node and timeout to dflopt

--- src/dflopt.c
+++ src/dflopt.c
@@ -1,2 +1,34 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <slash/slash.h>
+
 unsigned int slash_dfl_node = 0;
 unsigned int slash_dfl_timeout = 1000;
+
+static int cmd_node(struct slash *slash) {
+
+	if (slash->argc >= 1) {
+
+		/* We rely on user to provide known hosts implemetnation */
+		int known_hosts_get_node(char * find_name);
+		slash_dfl_node = known_hosts_get_node(slash->argv[1]);
+		if (slash_dfl_node == 0)
+			slash_dfl_node = atoi(slash->argv[1]);
+	}
+
+	return SLASH_SUCCESS;
+}
+slash_command(node, cmd_node, "<node>", NULL);
+
+
+static int cmd_timeout(struct slash *slash) {
+
+	if (slash->argc >= 1) {
+		slash_dfl_timeout = atoi(slash->argv[1]);
+	}
+
+    printf("timeout = %d\n", slash_dfl_timeout);
+
+	return SLASH_SUCCESS;
+}
+slash_command(timeout, cmd_timeout, "<timeout ms>", NULL);
