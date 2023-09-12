a792ca635c34849781c4f612bde29c04adcf7f1a
Troels Jessen <troels@space-inventor.com>
2023-09-12 09:53:44 +0200

    SI-3024 Add default RDP options

--- include/slash/dflopt.h
+++ include/slash/dflopt.h
@@ -1,5 +1,19 @@
+#pragma once
+
+#include <slash/slash.h>
+#include <slash/optparse.h>
 
 
 /* Global default arguments */
 extern unsigned int slash_dfl_node;
 extern unsigned int slash_dfl_timeout;
+
+extern unsigned int rdp_dfl_window;
+extern unsigned int rdp_dfl_conn_timeout;
+extern unsigned int rdp_dfl_packet_timeout;
+extern unsigned int rdp_dfl_ack_timeout;
+extern unsigned int rdp_dfl_ack_count;
+
+void rdp_opt_add(optparse_t * parser);
+void rdp_opt_set();
+void rdp_opt_reset();
